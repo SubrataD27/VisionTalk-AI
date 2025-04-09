5adc74052e75b218e96b7c7357f075f8f84230aa
SubrataD27 <dhibarsubrata18@gmail.com>
2025-04-10 02:20:07 +0530

    FINALdd

--- app.py
+++ app.py
@@ -183,4 +183,5 @@ def image_question():
         return jsonify({"success": False, "error": str(e)})
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=5000)
